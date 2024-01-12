9bcf48b5952eeff9daaab01a9aaceb3b2a84751c
Justin Gerard Ricardo <justinericardo@gmail.com>
2024-01-12 22:46:47 +0800

    Define script for transcribing audio from a microphone

--- modules/transcribe_mic_audio.py
+++ modules/transcribe_mic_audio.py
@@ -29,10 +29,30 @@ def on_close():
     print("Closing Session")
 
 
-def main(args):
-    transcriber = aai.Transcriber()
-    transcript = transcriber.transcribe(args["audio_file"])
-    print(f"Mic audio transcription:\n{transcript.text}")
+def main():
+    transcriber = aai.RealtimeTranscriber(
+        sample_rate=16_000,
+        on_data=on_data,
+        on_error=on_error,
+        on_open=on_open,
+        on_close=on_close,
+    )
+
+    # Connect to AssemblyAI's real-time transcription service via WebSockets
+    transcriber.connect()
+
+    # Open up a microphone stream with a sample rate of 16_000
+    microphone_stream = aai.extras.MicrophoneStream(sample_rate=16_000)
+
+    # Send microphone stream to transcriber service
+    # until end/stop of recording (trigger by Ctrl+C)
+    transcriber.stream(microphone_stream)
+
+    # Close the stream to the transcriber
+    transcriber.close()
+
+    # Just in case, close the microphone stream as well
+    microphone_stream.close()
 
 
 if __name__ == "__main__":
