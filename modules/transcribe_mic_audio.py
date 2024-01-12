590ae63350255b9164fa2fa9041968c247944420
Justin Gerard Ricardo <justinericardo@gmail.com>
2024-01-12 22:41:38 +0800

    Add event handlers for AssemblyAI's transcriber

--- modules/transcribe_mic_audio.py
+++ modules/transcribe_mic_audio.py
@@ -7,6 +7,28 @@ import assemblyai as aai
 aai.settings.api_key = os.getenv("AAI_API_KEY")
 
 
+def on_open(session_opened: aai.RealtimeSessionOpened):
+    print("Session ID:", session_opened.session_id)
+
+
+def on_data(transcript: aai.RealtimeTranscript):
+    if not transcript.text:
+        return
+
+    if isinstance(transcript, aai.RealtimeFinalTranscript):
+        print(transcript.text, end="\r\n")
+    else:
+        print(transcript.text, end="\r")
+
+
+def on_error(error: aai.RealtimeError):
+    print("An error occured:", error)
+
+
+def on_close():
+    print("Closing Session")
+
+
 def main(args):
     transcriber = aai.Transcriber()
     transcript = transcriber.transcribe(args["audio_file"])
